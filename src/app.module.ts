99efeb6e96963e16f6fbf0728e4325929e8806e7
JulzDave <44776240+JulzDave@users.noreply.github.com>
2023-02-06 00:50:22 +0200

    deleted `app.*.ts` stock boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [UsersModule, AuthModule, ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
